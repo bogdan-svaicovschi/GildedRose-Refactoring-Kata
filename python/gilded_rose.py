eb50fa9cf9ffb1ce59ef0a112f9d18141dea210b
Bogdan Svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-09 17:44:11 +0300

    optimized code

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,71 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
+    BRIE = "Aged Brie"
+    CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+    SULFUR = "Sulfuras, Hand of Ragnaros"
 
     def __init__(self, items):
         self.items = items
 
+    # def update_quality(self):
+    #     for item in self.items:
+    #         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+    #             if item.quality > 0:
+    #                 if item.name != "Sulfuras, Hand of Ragnaros":
+    #                     item.quality = item.quality - 1
+    #         else:
+    #             if item.quality < 50:
+    #                 item.quality = item.quality + 1
+    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.sell_in < 11:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #                     if item.sell_in < 6:
+    #                         if item.quality < 50:
+    #                             item.quality = item.quality + 1
+    #         if item.name != "Sulfuras, Hand of Ragnaros":
+    #             item.sell_in = item.sell_in - 1
+    #         if item.sell_in < 0:
+    #             if item.name != "Aged Brie":
+    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
+    #                     if item.quality > 0:
+    #                         if item.name != "Sulfuras, Hand of Ragnaros":
+    #                             item.quality = item.quality - 1
+    #                 else:
+    #                     item.quality = item.quality - item.quality
+    #             else:
+    #                 if item.quality < 50:
+    #                     item.quality = item.quality + 1
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
+            if item.name != self.SULFUR:
+                if item.name != self.CONCERT and item.name != self.BRIE:
+                    item.quality = max(0, item.quality - 1)
                 else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    additional = 1
+                    if item.name == self.CONCERT:
+                        if item.sell_in < 11:
+                            if item.sell_in < 6:
+                                additional += 2
+                            else:
+                                additional += 1
+                    item.quality = min(50, item.quality + additional)
+
+                item.sell_in -= 1
+
+                if item.sell_in < 0:
+                    if item.name == self.CONCERT:
+                        item.quality = 0
+                    elif item.name == self.BRIE:
+                        item.quality = min(50, item.quality + 1)
+                    else:
+                        item.quality = max(0, item.quality - 1)
+
+
+                    
+
 
 
 class Item:
