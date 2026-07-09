9f5a1c0c915a3e29ff299636f70b84cd97e13f44
Bogdan Svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-09 18:00:46 +0300

    fixed magic numbers

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,6 +5,12 @@ class GildedRose(object):
     CONCERT = "Backstage passes to a TAFKAL80ETC concert"
     SULFUR = "Sulfuras, Hand of Ragnaros"
     CONJURED = "Conjured"
+    UNIT = 1
+    DOUBLE_UNIT = 2
+    LOWER_BOUND = 0
+    UPPER_BOUND = 50
+    FIRST_THRESHOLD = 10
+    SECOND_THRESHOLD = 5
 
     def __init__(self, items):
         self.items = items
@@ -13,30 +19,30 @@ class GildedRose(object):
         for item in self.items:
             if item.name != self.SULFUR:
                 if item.name != self.CONCERT and item.name != self.BRIE:
-                    item.quality = max(0, item.quality - 1)
+                    item.quality = max(self.LOWER_BOUND, item.quality - self.UNIT)
                     if item.name == self.CONJURED:
-                        item.quality = max(0, item.quality - 1)
+                        item.quality = max(self.LOWER_BOUND, item.quality - self.UNIT)
                 else:
-                    additional = 1
+                    additional = self.UNIT
                     if item.name == self.CONCERT:
-                        if item.sell_in < 11:
-                            if item.sell_in < 6:
-                                additional += 2
+                        if item.sell_in <= self.FIRST_THRESHOLD:
+                            if item.sell_in <= self.SECOND_THRESHOLD:
+                                additional += self.DOUBLE_UNIT
                             else:
-                                additional += 1
-                    item.quality = min(50, item.quality + additional)
+                                additional += self.UNIT
+                    item.quality = min(self.UPPER_BOUND, item.quality + additional)
 
-                item.sell_in -= 1
+                item.sell_in -= self.UNIT
 
-                if item.sell_in < 0:
+                if item.sell_in < self.LOWER_BOUND:
                     if item.name == self.CONCERT:
-                        item.quality = 0
+                        item.quality = self.LOWER_BOUND
                     elif item.name == self.BRIE:
-                        item.quality = min(50, item.quality + 1)
+                        item.quality = min(self.UPPER_BOUND, item.quality + self.UNIT)
                     else:
-                        item.quality = max(0, item.quality - 1)
+                        item.quality = max(self.LOWER_BOUND, item.quality - self.UNIT)
                         if item.name == self.CONJURED:
-                            item.quality = max(0, item.quality - 1)
+                            item.quality = max(self.LOWER_BOUND, item.quality - self.UNIT)
 
 
                     
