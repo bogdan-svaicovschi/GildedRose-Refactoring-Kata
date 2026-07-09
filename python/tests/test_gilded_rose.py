ed4b89dc512b76015a174a04b8dd3de7c6fc5978
Bogdan Svaicovschi <bogdan.svaicovschi@softwire.com>
2026-07-09 17:23:35 +0300

    renamed test cases and fixed comments

--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -3,88 +3,82 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+NORMAL = "cheese"
+BRIE = "Aged Brie"
+CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+SULFUR = "Sulfuras, Hand of Ragnaros"
 
 class GildedRoseTest(unittest.TestCase):
     def test_name(self):
-        items = [Item("foo", 10, 10)]
+        items = [Item(NORMAL, 10, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("foo", items[0].name)
+        self.assertEqual(NORMAL, items[0].name)
 
     def test_update_normal_item(self):
-        items = items = [Item("foo", 10, 10)]
+        items = items = [Item(NORMAL, 10, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
+        self.assertEqual(9, items[0].quality)
+
+    def test_update_normal_item_after_sellin_expired(self):
+        items = items = [Item(NORMAL, 0, 10)]
+        gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEqual(8, items[0].quality)
 
-    def test_update_normal_item_sellin_expired(self):
-        items = items = [Item("foo", 0, 10)]
+    def test_update_check_if_it_maxes_at_50(self):
+        items = items = [Item(BRIE, 10, 50)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(6, items[0].quality)
-
-    def test_update_brie(self):
-        items = items = [Item("Aged Brie", 10, 50)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         gilded_rose.update_quality()
         self.assertEqual(50, items[0].quality)
 
-    def test_update_brie_50(self):
-        items = items = [Item("Aged Brie", 10, 24)]
+    def test_update_brie(self):
+        items = items = [Item(BRIE, 10, 24)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(26, items[0].quality)
+        self.assertEqual(25, items[0].quality)
 
-    def test_brie_quality(self):
-        items = items = [Item("Aged Brie", 1, 46)]
+    def test_brie_quality_increases_by_two_after_sellin(self):
+        items = items = [Item(BRIE, 0, 46)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(49, items[0].quality)
+        self.assertEqual(48, items[0].quality)
 
     def test_sulfuras(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", 10, 49)]
+        items = [Item(SULFUR, 10, 49)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         gilded_rose.update_quality()
         self.assertEqual(49, items[0].quality)
         self.assertEqual(10, items[0].sell_in)
 
     def test_ticket(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 20, 38)]
+        items = [Item(CONCERT, 20, 38)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(40, items[0].quality)
+        self.assertEqual(39, items[0].quality)
     
     def test_ticket_sellin1(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 36)]
+        items = [Item(CONCERT, 10, 36)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(40, items[0].quality)
+        self.assertEqual(38, items[0].quality)
 
     def test_ticket_sellin2(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 24)]
+        items = [Item(CONCERT, 5, 24)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        gilded_rose.update_quality()
-        self.assertEqual(30, items[0].quality)
+        self.assertEqual(27, items[0].quality)
 
     def test_ticket_sellin0(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 24)]
+        items = [Item(CONCERT, 0, 24)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
         gilded_rose.update_quality()
         self.assertEqual(0, items[0].quality)
 
     def test_repr(self):
-        items = [Item("foo", 10, 10)]
-        self.assertEqual("foo, 10, 10", items[0].__repr__())
+        items = [Item(NORMAL, 10, 10)]
+        self.assertEqual("cheese, 10, 10", items[0].__repr__())
     
     
 
